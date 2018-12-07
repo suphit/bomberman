d53283b78033cdfc1ef0cfa7b9170806f19aeae4
suphit <souphitpmc@gmail.com>
2018-12-07 08:50:10 +0700

    game

---
+++ src/uet/oop/bomberman/entities/character/enemy/ai/AIMedium.java
@@ -0,0 +1,33 @@
+package uet.oop.bomberman.entities.character.enemy.ai;
+
+import uet.oop.bomberman.entities.character.Bomber;
+import uet.oop.bomberman.entities.character.enemy.Enemy;
+
+public class AIMedium extends AI {
+	Bomber _bomber;
+	Enemy _e;
+	
+	public AIMedium(Bomber bomber, Enemy e) {
+		_bomber = bomber;
+		_e = e;
+	}
+
+	@Override
+	public int calculateDirection() {
+		// TODO: cài đặt thuật toán tìm đường đi
+
+		if(_bomber.getXTile()==_e.getXTile()){
+			if(_bomber.getYTile()<_e.getYTile())
+				return 0;
+			else return 2;
+		}
+		if(_bomber.getYTile()== _e.getYTile()){
+			if(_bomber.getXTile()>_e.getXTile()){
+				return 1;
+			}
+			else return 3;
+		}
+		return random.nextInt(4);
+	}
+
+}
---
+++ src/uet/oop/bomberman/entities/tile/Portal.java
@@ -0,0 +1,28 @@
+package uet.oop.bomberman.entities.tile;
+
+import uet.oop.bomberman.Board;
+import uet.oop.bomberman.entities.Entity;
+import uet.oop.bomberman.entities.character.Bomber;
+import uet.oop.bomberman.entities.character.Character;
+import uet.oop.bomberman.graphics.Sprite;
+
+public class Portal extends Tile {
+	protected Board _board;
+
+
+	public Portal(int x, int y, Sprite sprite,Board board ) {
+		super(x, y, sprite);
+		_board=board;
+	}
+	
+	@Override
+	public boolean collide(Entity e) {
+		// TODO: xử lý khi Bomber đi vào
+		if(e instanceof Bomber ){
+			if(_board.detectNoEnemies())
+				_board.nextLevel();
+		}
+		return true;
+	}
+
+}
